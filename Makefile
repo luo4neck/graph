ae0fb098c77673715844d8b7338f97ea435b0b92
luo4neck <luo4neck@gmail.com>
2014-02-25 23:36:00 +0000

    2nd

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 graph: graph.o
-	gcc -Wall -o graph graph.o 
+	gcc -Wall -o graph graph.o -lm 
 
 graph.o: graph.c 
-	gcc -Wall -c graph.c
+	gcc -Wall -c graph.c -lm
 
 test: graph
 	./graph read.dat
 
 clean:
-	rm -f graph.o 
+	rm -f core* graph.o 
--- graph.c
+++ graph.c
@@ -1,15 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void free_graph(int **p, int vecnum)
+{
+int i;
+for(i=0; i<vecnum; i++)
+    {
+    free(p[i]);
+ //   printf("fuck %d\n", i);
+    }
+//printf("fuck all\n");
+free(p);
+//printf("fuck all\n");
+}
+
+
 int main(int argc, char *argv[])
 {
-FILE *fp;
-char c;
-int i=0, j=0,k=0, vecnum = 0, v=0, link[], start[];
-
-
 if(argc>2)
     {printf("Please input only file name!\n");}
+
+FILE *fp;
+char c;
+int i=0, j=0, vecnum = 0;
 
 if((fp = fopen(argv[1], "rb")) == NULL)
     {
@@ -20,20 +33,59 @@ if((fp = fopen(argv[1], "rb")) == NULL)
 while(!feof(fp))
     {
     c = getc(fp);
-    if(c>47 && c<58)
-       {
-       link[i] = c-48;
-       j++;
-       }
-    else if(c ==10)
-       {
-       start[k]=j;
-       k++;
-       j=0;
-       }
+    if ( c==10 )  vecnum++;
+    }
+//printf("%d\n",vecnum);
+
+int **p;
+p=(int **)malloc(vecnum*sizeof(int *));
+for(i=0; i<vecnum; i++)
+    { 
+    p[i] = (int *)malloc(sizeof(int)*vecnum); 
+    for(j=0; j<vecnum; j++)
+        {
+        p[i][j] = -1;
+    //    printf("%d, ", p[i][j]);
+        }
+  //  printf("\n");
     }
 
-printf("%d\n", k);
+//printf("fuck all\n");
+fp = fopen(argv[1], "rb");
+i=j=0;
+while(!feof(fp))
+    {
+    c = getc(fp);
+    if( c>47 && c<58)
+        {
+        p[i][j] = c-48;
+  //  printf("%d, ", p[i][j]);
+        j++;
+        }
+    if ( c==10 ) 
+        {
+        i++;
+        j=0;
+ //       printf("\n");
+        }// vecnum++;
+    }
+free(fp);
 
+for(i=0; i<vecnum; i++)
+    {
+    j=0;
+    while( p[i][j] >= 0 )
+        {
+
+    printf("%d, ", p[i][j]);
+        j++;
+        }
+        printf("\n");
+    }
+
+
+
+//printf("fuck all\n");
+free_graph(p, vecnum);
 return 0;
 }
